ff86a17ae5e97de308234aa20e77981b56fbd2f1
Z0XM <mukulz2020@gmail.com>
2023-02-20 06:54:02 +0000

    Deploying to gh-pages from @ gdscwow/GDSC-WOW-23-TS@eb068e51a85e6595e1e357a3c4fbba9e37d4c84b 🚀

---
+++ _next/static/chunks/pages/[region]-10475fdc97f0ff87.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[16],{7724:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[region]",function(){return _(3753)}])},3753:function(n,u,_){"use strict";_.r(u);var t=_(4588);u.default=t.Z}},function(n){n.O(0,[107,588,774,888,179],function(){return n(n.s=7724)}),_N_E=n.O()}]);
