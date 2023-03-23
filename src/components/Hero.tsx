190c8503b243566f35c055040ef4d9e41f582d71
Mukul <mukulz2020@gmail.com>
2023-03-23 13:06:25 +0530

    bbsr custom

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,6 +43,55 @@ const RegionNotFound = ({ currentRegion }: { currentRegion: string | null | unde
 };
 
 const RegionFound = ({ currentRegion }: { currentRegion: Region | null | undefined }) => {
+	if (currentRegion?.name === 'Bhubaneshwar') {
+		return (
+			<div className='w-screen mb-16 sm:mb-20'>
+				<div className='w-full h-full flex flex-row'>
+					<div className='flex flex-col items-start justify-center mx-24'>
+						<p className='hidden min-[1537px]:block mb-5 min-[1537px]:mt-8 text-2xl font-semibold text-center text-gblue'>
+							Hello, We Welcome you to
+						</p>
+						<Image src='/svg/wowcenter.svg' width={300} height={300} alt='circle' />
+						<div className='uppercase text-transparent bg-gradient-to-r bg-clip-text from-[#2E2525] to-[#733423] text-justify text-[5vw] font-google-sans font-bold'>
+							{currentRegion?.name}
+						</div>
+					</div>
+					<div className='flex flex-col items-center justify-center mx-24'>
+						<div className='mb-4 min-[1537px]:mb-8 w-fit group'>
+							<button className='flex items-center px-6 py-3 mt-5 text-xl rounded-md cursor-pointer w-fit bg-gblue'>
+								<span className={`${poppins.className} text-white`}>
+									{currentRegion?.registration ? (
+										<Link href={currentRegion.registration}>Register Now !</Link>
+									) : (
+										`REGISTRATIONS OPENING SOON !`
+									)}
+								</span>
+							</button>
+							<div className='mt-2 h-[2px] transition-all group-hover:bg-ggreen w-full bg-transparent'></div>
+						</div>
+						<div className={`mb-4 min-[1537px]:mb-8 text-xl sm:text-3xl text-center ${poppins.className}`}>
+							<div>Date: {currentRegion?.date || 'To Be Announced'}</div>
+							<div className='mt-2 h-[2px] transition-all bg-gred w-full'></div>
+						</div>
+						<div className={`text-center text-lg sm:text-2xl ${poppins.className}`}>
+							{currentRegion?.map && currentRegion.venue ? (
+								<Link href={currentRegion.map}>
+									<div>Venue: {currentRegion.venue}</div>
+								</Link>
+							) : (
+								<div>Venue: {currentRegion?.venue || 'Coming Soon'}</div>
+							)}
+
+							<div className='mt-2 h-[2px] transition-all bg-gyellow w-full'></div>
+						</div>
+					</div>
+				</div>
+				<div className='relative w-full '>
+					<Image src='/png/bbsr.png' width={1440} height={380} alt='Bhubaneshwar' className='relative w-full' />
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div className='w-screen mb-16 sm:mb-20'>
 			{/* <Image
