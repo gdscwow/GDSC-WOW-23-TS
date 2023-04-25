95c450d42d8b99580479ffd14a2aea248c8bdfec
Mukul <mukulz2020@gmail.com>
2023-04-25 20:07:11 +0530

    hyd sponsor

--- src/components/Sponsor.tsx
+++ src/components/Sponsor.tsx
@@ -56,6 +56,29 @@ const Sponsor = () => {
 				</div>
 			</div>
 		);
+	if (regionData.current?.name === 'Hyderabad')
+		return (
+			<div className='relative flex flex-col mt-10 items-center w-screen font-google-sans'>
+				<div className='relative flex flex-col items-center divide-y-[1px] divide-ggreen p-10 mx-10 w-[80%] text-lg border-2 rounded-lg sm:mx-24 border-gblue'>
+					<div className='flex items-center flex-col h-full w-full pb-8'>
+						{/* <div className='font-semibold text-2xl mt-8 mb-4 text-ggreen'>Sponsors</div> */}
+						<div className='flex flex-row h-full items-end w-full justify-evenly'>
+							<div className='flex flex-col h-full justify-evenly gap-4 items-center'>
+								<div className='w-[200px] mt-16 mb-4'>
+									<Link href='http://www.elitceler.com/'>
+										<Image src='/hyd/hyd.png' alt='ElitCeler Technologies' width={400} height={400} />
+									</Link>
+								</div>
+								<div className='font-semibold '>ElitCeler Technologies</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='absolute px-24 py-2 text-3xl font-medium text-white rounded-lg bg-gblue top-[-3%]'>
+					Sponsors
+				</div>
+			</div>
+		);
 	return <></>;
 };
 
