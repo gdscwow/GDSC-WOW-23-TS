d448cbcd6440aa2f18da70b54d99c4e2c5931b49
Mukul <mukulz2020@gmail.com>
2023-04-26 19:58:52 +0530

    hyd sponsor

--- src/components/Sponsor.tsx
+++ src/components/Sponsor.tsx
@@ -62,14 +62,28 @@ const Sponsor = () => {
 				<div className='relative flex flex-col items-center divide-y-[1px] divide-ggreen p-10 mx-10 w-[80%] text-lg border-2 rounded-lg sm:mx-24 border-gblue'>
 					<div className='flex items-center flex-col h-full w-full pb-8'>
 						{/* <div className='font-semibold text-2xl mt-8 mb-4 text-ggreen'>Sponsors</div> */}
-						<div className='flex flex-row h-full items-end w-full justify-evenly'>
+						<div className='flex flex-row h-full items-end w-full justify-evenly flex-wrap'>
 							<div className='flex flex-col h-full justify-evenly gap-4 items-center'>
-								<div className='w-[200px] mt-16 mb-4'>
+								<div className='w-[200px] mt-8 mb-4'>
 									<Link href='http://www.elitceler.com/'>
 										<Image src='/hyd/hyd.png' alt='ElitCeler Technologies' width={400} height={400} />
 									</Link>
 								</div>
 								<div className='font-semibold '>ElitCeler Technologies</div>
+							</div>
+							<div className='flex flex-col h-full justify-evenly  items-center'>
+								<div className='w-[200px] mt-8 mb-4'>
+									<Link href='http://newbielearning.com'>
+										<Image src='/hyd/hyd2.png' alt='Newbie' width={400} height={400} />
+									</Link>
+								</div>
+								<div className='font-semibold mt-[-60px]'>Newbie</div>
+							</div>
+							<div className='flex flex-col h-full justify-evenly  items-center'>
+								<div className='w-[200px] mt-8 mb-4'>
+									<Image src='/hyd/hyd3.png' alt='CreatX' width={400} height={400} />
+								</div>
+								<div className='font-semibold mt-[-60px]'>CreatX</div>
 							</div>
 						</div>
 					</div>
